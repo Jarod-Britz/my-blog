e234eb241286a92d24386d7891f63f474a76f6cf
Jarod-Britz <jarodbritz@gmail.com>
2018-08-01 11:03:08 +0200

    sign in added

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,34 @@ var request = require('request');
 
 /* GET sign in. */
 router.get('/', function (req, res, next) {
-  console.log(posts);
-
   res.render('signin', {
     title: "Signin",
-    posts: posts.posts
+    posts: posts.posts,
+    users: posts.users
   });
+
+});
+router.post('/', function (req, res, next) {
+  var users = posts.users;
+  console.log(users);
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  console.log("Username: "+username+"======="+" Password: "+password);
+  
+  for (let i = 0; i < users.length-1; i++) {
+    const user = users[i];
+    console.log(user);
+        if (username === user.username &&  password == user.password) {
+      res.redirect('/index');
+    } else {
+      console.log("Username or Password is incorrect");
+    }
+  
+  }
+
+
 });
 
 
@@ -69,78 +91,78 @@ router.post('/new', function (req, res, next) {
     json: true
 
   }, function (error, response, body) {
-   
-  res.render('index', {
-    title: "Home",
-    posts: posts.posts
-  });
+
+    res.render('index', {
+      title: "Home",
+      posts: posts.posts
+    });
 
   });
 
 });
 
 //VIEW POST
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   //make a post request to our database
   request({
-  uri: "http://localhost:8000/posts/" + req.params.id,
-  method: "GET",
-  }, function(error, response, body) {
-      console.log(JSON.parse(body));
-      //send a response message
-      res.render('view', {posts: JSON.parse(body)});
+    uri: "http://localhost:8000/posts/" + req.params.id,
+    method: "GET",
+  }, function (error, response, body) {
+    console.log(JSON.parse(body));
+    //send a response message
+    res.render('view', { posts: JSON.parse(body) });
   });
 })
 
 
 //DELETE BUTTON
-router.get('/delete/:id', function(req, res, next) {
+router.get('/delete/:id', function (req, res, next) {
   console.log(req.params.id)
-  
-request({
-  uri: "http://localhost:8000/posts/"  + req.params.id,
-  method: "DELETE",
-  }, function(error, response, body) {
 
-      let data = { 
-          message: 'Successfully Removed.',
-      }
+  request({
+    uri: "http://localhost:8000/posts/" + req.params.id,
+    method: "DELETE",
+  }, function (error, response, body) {
 
-      res.redirect('/index');
+    let data = {
+      message: 'Successfully Removed.',
+    }
+
+    res.redirect('/index');
   });
 });
 
 
 // UPDATE ROUTES
-router.get('/update/:id', function(req, res, next) {
+router.get('/update/:id', function (req, res, next) {
 
   //make a post request to our database
   request({
-  uri: "http://localhost:8000/posts/" + req.params.id,
-  method: "GET",
-  }, function(error, response, body) {
-      console.log(JSON.parse(body));
-      //send a response message
-      res.render('update', {message: false, posts: JSON.parse(body)});
+    uri: "http://localhost:8000/posts/" + req.params.id,
+    method: "GET",
+  }, function (error, response, body) {
+    console.log(JSON.parse(body));
+    //send a response message
+    res.render('update', { message: false, posts: JSON.parse(body) });
   });
- 
- });
- 
- router.post('/update/:id', function(req, res, next) {
+
+});
+
+router.post('/update/:id', function (req, res, next) {
   request({
     uri: "http://localhost:8000/posts/" + req.params.id,
-  method: "PATCH",
-  form: {
+    method: "PATCH",
+    form: {
       title: req.body.title,
       content: req.body.content,
       author: req.body.author
-  }
-  }, function(error, response, body) {
-      // console.log(body);
-      //send a response message
-      res.render('update', {message: 'Successfully Changed.', posts: JSON.parse(body)});
+    }
+  }, function (error, response, body) {
+    // console.log(body);
+    //send a response message
+    res.render('update', { message: 'Successfully Changed.', posts: JSON.parse(body) });
   });
- });
+});
 
 
 module.exports = router;
